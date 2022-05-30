2b3a7c396afed1df317058536c690b0b3f4b667b
driehoeken <a.bednarski1203@gmail.com>
2022-05-30 19:44:50 +0200

    drag and drop can change position in list

--- src/script/dragdrop.js
+++ src/script/dragdrop.js
@@ -13,7 +13,11 @@ const dragOver = function(e){
     const tasksWrapper = e.target.closest('.tasks');
     e.preventDefault();
     const dragging = document.querySelector('.dragging');
-    tasksWrapper.appendChild(dragging);
+    //tasksWrapper.appendChild(dragging);
+    const rect = tasksWrapper.getBoundingClientRect();
+    const lengthFromTop = e.clientY - rect.y;
+    const dragIndex = Math.floor((lengthFromTop + 40) / 90);
+    tasksWrapper.insertBefore(dragging, [...tasksWrapper.children][dragIndex]);
 }
 
 columnsContainer.addEventListener('dragstart', dragStart);
